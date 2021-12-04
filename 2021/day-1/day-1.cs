dfedebc7bf22354dee8b1b651a6fca9b44f9da35
Nikolas Orkic <niorkic@gmail.com>
2021-12-04 17:21:50 -0500

    day-1-task-2

--- 2021/day-1/day-1.cs
+++ 2021/day-1/day-1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -12,31 +13,80 @@ namespace AdventOfCode {
                 Console.ReadLine();
                 Environment.Exit(1);
             }
-
+            var projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
             var fileName = args[0];
-            var lines = File.ReadAllLines(fileName).ToList();
 
-            Console.WriteLine(args[0]);
+            var lines = File.ReadAllLines(projectDirectory + fileName).ToList();
 
+           
+            
+            
+
+            Console.WriteLine($"Beginning regular increases");
+
+            Console.WriteLine($"Number of increases {regularIncreases(lines)}");
+
+
+            Console.WriteLine($"Beginning Sliding Window Increases");
+            Console.WriteLine($"Number of increases {slidingWindowIncreases(lines, 3)}");
+
+
+            Console.WriteLine("Exiting...");
+            Console.ReadKey();
+        }
+
+        public static int regularIncreases(List<string> lines)
+        {
             int? previousDepth = int.Parse(lines[0]);
-            lines.RemoveAt(0);
-
             int numIncreases = 0;
 
-            foreach (var line in  lines ) {
-                var currentDepth = int.Parse(line);
+            // element 0 has already been read
+            for (int i = 1; i<lines.Count; i++)
+            {
+                var currentDepth = int.Parse(lines[i]);
 
                 if (currentDepth > previousDepth)
                 {
                     numIncreases++;
-                }       
+                }
                 previousDepth = currentDepth;
             }
+            return numIncreases;
+        }
 
-            Console.WriteLine($"Number of increases {numIncreases}");
-            Console.WriteLine("Exiting...");
-            Console.ReadLine();
 
+        public static int slidingWindowIncreases(List<string> lines, int windowSize)
+        {
+            
+            int previousWindowSum = 0;
+            int numIncreases = 0;
+
+            Queue<int> windowQueue = new Queue<int>();
+
+            for (int i = 0; i < windowSize; i++)
+            {
+                var depth = int.Parse(lines[i]);
+                windowQueue.Enqueue(depth);
+                previousWindowSum += depth;
+            }
+
+
+            int lineNum = windowSize;
+            while (lineNum < lines.Count )
+            {
+                int depth = int.Parse(lines[lineNum]);
+                var currentWindowSum = previousWindowSum - windowQueue.Dequeue() + depth;
+                windowQueue.Enqueue(depth);
+
+
+                if (currentWindowSum > previousWindowSum)
+                {
+                    numIncreases++;
+                }
+                previousWindowSum = currentWindowSum;
+                lineNum++;
+            }
+            return numIncreases;
         }
 
 
